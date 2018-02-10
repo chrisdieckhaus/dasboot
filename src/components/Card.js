647974b43e45ded5242b076cc8cce53cb2afeabb
Chris <christopherdieckhaus@gmail.com>
2018-02-10 15:20:32 -0500

    Create basic card component

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../content/Card.less';
+
+class Card extends React.PureComponent {
+
+    render() {
+        return (
+			<div className="card-container">
+				<div className="card-title">
+					{this.props.title}
+				</div>
+				<div className="card-content">
+					{this.props.children}
+				</div>
+			</div>
+        )
+    }
+}
+
+Card.PropTypes = {
+    title: PropTypes.string,
+}
+
+export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,37 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import React from 'react';
+import Link from 'gatsby-link';
+import get from 'lodash/get';
+import Helmet from 'react-helmet';
 
-import Bio from '../components/Bio'
+import Card from '../components/Card';
 
-class BlogIndex extends React.Component {
+class Index extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+
+	const content = "Tenting. We spend weeks sleeping out in the cold, rain, and mud. We sacrifice sleep, grades, and social life. " + 
+	"And we do it all for the chance to squeeze ourselves into a tiny little gym with 1,600 of our classmates and watch ten other students toss a leather ball at a metal hoop.\n" +
+	"Is it crazy? Yes. Is it worth it? Absolutely./n" +
+	"You see, tenting is more than just an activity. It's more than just a game. Tenting is about passion and camaraderie, about dedication and sacrifice, " + 
+	"about ritual and tradition. It's more than just a game./n" +
+	"It's a way of life.";
+
 
     return (
-      <div>
+      <div className="container">
         <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
-        <Bio />
-
-		{posts.map(post => {
-			if (post.node.path !== '/404/') {
-				const title = get(post, 'node.frontmatter.title') || post.node.path
-				return (
-					<div key={post.node.frontmatter.path}>
-						<h3>
-							<Link to={post.node.frontmatter.path} >
-								{post.node.frontmatter.title}
-							</Link>
-						</h3>
-						<small>{post.node.frontmatter.date}</small>
-						<p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-					</div>
-				)
-			}
-		})}
+        <Card title="Card title">
+          {content}
+        </Card>
       </div>
     )
   }
 }
 
-BlogIndex.propTypes = {
+Index.propTypes = {
   route: React.PropTypes.object,
 }
 
-export default BlogIndex
+export default Index
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -65,3 +56,22 @@ export const pageQuery = graphql`
     }
   }
 `
+
+// const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+// const posts = get(this, 'props.data.allMarkdownRemark.edges')
+// {posts.map(post => {
+//   if (post.node.path !== '/404/') {
+//     const title = get(post, 'node.frontmatter.title') || post.node.path
+//     return (
+//       <div key={post.node.frontmatter.path}>
+//         <h3>
+//           <Link to={post.node.frontmatter.path} >
+//             {post.node.frontmatter.title}
+//           </Link>
+//         </h3>
+//         <small>{post.node.frontmatter.date}</small>
+//         <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+//       </div>
+//     )
+//   }
+// })}
