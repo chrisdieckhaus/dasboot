97088000a58e7924724f73339bf24f85610a957b
Chris <christopherdieckhaus@gmail.com>
2018-02-11 19:52:01 -0500

    Responsive header and cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,30 +6,30 @@ import '../content/Header.less';
 class Header extends React.Component {
     render() {
         return (
+			<div className="wrapper">
             <nav className="header-container">
                 <div className="menu-link">
                     <Link to="/">
                         Das Boot
                     </Link>
                 </div>
-                <div className="menu-link-container">
-                    <div className="menu-link">
-                        <Link to="/about">
-                            The Tent
-                        </Link>
-                    </div>
-                    <div className="menu-link">                    
-                        <Link to="/members">
-                            The Members
-                        </Link>
-                    </div>
-                    <div className="menu-link">
-                        <Link to="/contact">
-                            Contact
-                        </Link>
-                    </div>
-                </div>
+                <div className="menu-link">
+					<Link to="/about">
+						The Tent
+					</Link>
+				</div>
+				<div className="menu-link">                    
+					<Link to="/members">
+						The Members
+					</Link>
+				</div>
+				<div className="menu-link">
+					<Link to="/contact">
+						Contact
+					</Link>
+				</div>
             </nav>
+			</div>
         );
     }
 };
